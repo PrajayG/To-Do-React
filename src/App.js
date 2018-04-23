8eaee78457d3c8b8f1c0617811839655bd1885e7
Prajay Ghaghda <prajay.ghaghda@smartology.net>
2018-04-23 16:26:46 +0100

    Added delete button

--- src/App.js
+++ src/App.js
@@ -1,121 +1,97 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      list :['Hello', 'Whats going on'],
-      currentEntry : '',
-      value: '',
-    }
+      list: ["Hello", "Whats going on"],
+      currentEntry: "",
+      value: ""
+    };
 
-    this.addToDo = this.addToDo.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.addToDo = this.addToDo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
   }
 
-  
   addToDo(event) {
     const newList = [...this.state.list, this.state.value];
-    const newEntry = this.currentEntry
-    this.setState({list: newList});
-    this.setState({value: ''})
+    const newEntry = this.currentEntry;
+    this.setState({ list: newList });
+    this.setState({ value: "" });
   }
 
   handleChange(event) {
-    console.log('Hello')
-    this.setState({ value: event.target.value})
-    console.log(event.target.value)
+    console.log("Hello");
+    this.setState({ value: event.target.value });
+    console.log(event.target.value);
   }
 
+  deleteEntry(item) {
+    console.log(item)
+
+  }
 
   render() {
-
     return (
-      <div className="App">
-        <div className="container">
-          <div className="to-do-bar">
-            <TextEntry
-              className="TextEntry"
-              value = {this.state.value}
-              onChange ={this.handleChange}
-            />
-            <Submit 
-              className="SubmitButton"
-              name='Hello'
-              onClick={this.addToDo}
-            />
-          </div>
-            <List list={this.state.list}/>
-         
-        </div>
+      <div className="App container">
+        <TextEntry value={this.state.value} onChange={this.handleChange} />
+        <Submit name="Hello" onClick={this.addToDo} />
+        <List list={this.state.list} onClick={this.deleteEntry} />
       </div>
     );
   }
 }
 
-
 class TextEntry extends Component {
   render() {
     return (
-      <div className="textbox"> 
-        <input 
+      <div className="textbox">
+        <input
           type="text"
-          value= {this.props.value}
-          onChange = {this.props.onChange} />
+          value={this.props.value}
+          onChange={this.props.onChange}
+        />
       </div>
-    )
+    );
   }
 }
-
 
 class List extends Component {
   render() {
     return (
-      <div>  
-        {this.props.list.map(item => <p key={item.objectID}> {item}  <Delete /></p>)} 
-        
-      </div>  
-    )
+      <div>
+        {this.props.list.map(item => (
+          <div className="row">
+            <div className="one-half column">
+              <p key={item.objectID}> {item} </p> 
+            </div>
+            <div className="one-half column">
+              <button onClick={() => this.props.onClick(item)} >Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
 class Submit extends Component {
   render() {
     return (
-      <div className="submitbutton">
-        <button 
-          onClick={() => this.props.onClick()}
-        > 
-          {this.props.name}
-        </button>
-      </div>
-    )
-  }
-}
-
-class Delete extends Component {
-  render() {
-    return (
-      <div className="deletebutton">
-        <button>
-          Delete
-        </button>
-      </div>
-    )
+      <button onClick={() => this.props.onClick()}>
+        {this.props.name}
+      </button>
+    );
   }
 }
 
 export default App;
 
 // Component lists //
-
-
-App
-// text-entry // button
 
 // to-do-list
